7af1a270e541a1834bb1b01c7573efda8ba977ae
zangyunfei <zangyunfei@changba.com>
2019-03-29 19:14:21 +0800

    调换一下位置

--- HelloWorld/src/singletonpattern/SingleObject.java
+++ HelloWorld/src/singletonpattern/SingleObject.java
@@ -2,12 +2,12 @@ package singletonpattern;
 
 public class SingleObject {
 
+    //创建一个SingleObject类的静态私有实例对象
+    private static SingleObject instance = new SingleObject();
+
     //构造方法设为私有，这样SingleObject类就无法在其他类中被实例化
     private SingleObject() {
     }
-
-    //创建一个SingleObject类的静态私有实例对象
-    private static SingleObject instance = new SingleObject();
 
     //创建一个公共的静态方法，获取SingleObject类的唯一对象
     public static SingleObject getInstance() {
